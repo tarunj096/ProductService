2ad9009d6564be6c7afa4dc1a2e721d590d94f77
Tarun J <tarunj096@gmail.com>
2023-12-06 00:06:53 +0530

    sql connection

--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,16 +1,20 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
 
     private String title;
-    private String desc;
+    private String description;
     private int price;
     private String image;
+    @ManyToOne
     private Category category;
 
 
